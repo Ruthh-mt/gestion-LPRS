f55c7f25a97d70f6b5d83fac546b2d658f5ea9b0
Beldendy <ruthmetayer0@gmail.com>
2026-01-06 11:05:36 +0100

    feat : ajout du squelette de l'inscription

---
+++ src/main/java/appli/InscriptionController.java
@@ -0,0 +1,37 @@
+package appli;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+
+public class InscriptionController {
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private PasswordField mdpConfirmationField;
+
+    @FXML
+    private PasswordField mdpField;
+
+    @FXML
+    private TextField nomField;
+
+    @FXML
+    private TextField prenomField;
+
+    @FXML
+    void onInscriptionClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onConnexionClick(ActionEvent event) throws IOException {
+        StartApplication.changeScene("accueil/login");
+    }
+
+}
