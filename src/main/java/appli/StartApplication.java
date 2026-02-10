de333572ae924b024be17d5edd421e2bb0dd0159
Augustin-Erceville <augustinderceville@gmail.com>
2026-02-10 08:54:09 +0100

    gnn - Je sais plus ce que j'ai fais

--- src/main/java/appli/StartApplication.java
+++ src/main/java/appli/StartApplication.java
@@ -13,7 +13,7 @@ public class    StartApplication extends Application {
     public void start(Stage stage) throws IOException {
         mainStage = stage;
         FXMLLoader fxmlLoader = new FXMLLoader(StartApplication.class.getResource("accueil/loginView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 500, 500);
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         mainStage.setTitle("Connexion");
         mainStage.setScene(scene);
         mainStage.show();
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -14,4 +14,6 @@ module appli{
     exports appli.accueil;
     exports appli.secretaire;
     opens appli.secretaire to javafx.fxml;
+    opens appli.profil to javafx.fxml;
+    exports appli.profil;
 }
