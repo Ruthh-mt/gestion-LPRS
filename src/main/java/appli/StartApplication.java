8ba8ed9d9698fdc624e0cad727d614170534009a
Aereises <ruthmetayer0@gmail.com>
2026-02-17 11:33:35 +0100

    feat:ajout d'une methode pour transferrer des donner vers des controller

--- src/main/java/appli/StartApplication.java
+++ src/main/java/appli/StartApplication.java
@@ -22,8 +22,12 @@ public class    StartApplication extends Application {
         FXMLLoader fxmlLoader = new
                 FXMLLoader(StartApplication.class.getResource(nomDuFichierFxml + "View.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+        scene.setUserData(fxmlLoader.getController());
         mainStage.setScene(scene);
         mainStage.setTitle(namePage);
+    }
+    public static Object getControllerFromStage(){
+        return mainStage.getScene().getUserData();
     }
     public static void main(String[] args) {
         launch();
