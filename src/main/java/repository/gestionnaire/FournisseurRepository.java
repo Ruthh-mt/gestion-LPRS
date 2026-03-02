deb07b0b3133085815ab47acb9560049d7663992
Aereises <ruthmetayer0@gmail.com>
2026-03-02 11:31:07 +0100

    feat: finalisation du CRUD des fournisseur

--- src/main/java/repository/gestionnaire/FournisseurRepository.java
+++ src/main/java/repository/gestionnaire/FournisseurRepository.java
@@ -1,6 +1,7 @@
 package repository.gestionnaire;
 
 import database.Database;
+import model.Utilisateur;
 import model.gestionnaire.Fournisseur;
 
 import java.sql.Connection;
@@ -34,9 +35,6 @@ public class FournisseurRepository {
         }
 
     }
-    public void getNameFournisseurById(int idFournisseur) {
-
-    }
 
     public ArrayList<Fournisseur> getAllFournisseur() {
         String getAll="SELECT * FROM fournisseur";
@@ -65,8 +63,63 @@ public class FournisseurRepository {
 
     }
 
+    public Fournisseur getFournisseurByMail(Fournisseur fournisseur) {
+        String get = "SELECT* FROM fournisseur WHERE mail_fournisseur=? ";
+        try {
+            PreparedStatement stmt = this.cnx.prepareStatement(get);
+            stmt.setString(1, fournisseur.getMailFournisseur());
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                fournisseur= new Fournisseur (
+                        rs.getInt("id_fournisseur"),
+                        rs.getString("nom_fournisseur"),
+                        rs.getString("adresse_fournisseur"),
+                        rs.getString("mail_fournisseur"),
+                        rs.getString("telephone_fournisseur"),
+                        rs.getInt("delai_livraison_moyen"),
+                        rs.getDouble("frais_livraison")
+                );
+            }
+            return fournisseur;
+        } catch (SQLException e) {
+            System.out.println("Erreur lors de la recherche du fournisseur: " + '\n' + " >>" + e.getMessage());
+            return null;
+        }
+    }
 
 
+    public boolean supprimerFournisseurParId(Fournisseur fournisseur) {
+        String delete = "DELETE FROM fournisseur WHERE id_fournisseur=? ";
+        try {
+            PreparedStatement stmt = this.cnx.prepareStatement(delete);
+            stmt.setInt(1, fournisseur.getIdFournisseur());
+            stmt.executeUpdate();
+            System.out.println("Fournisseur deleted with success : " + '\n' + ">> Fournisseur was : " + fournisseur.getNomfournisseur());
+            return true;
+        } catch (SQLException e) {
+            System.out.println("Erreur lors de la suppresion : " + '\n' + " >>" + e.getMessage());
+            return false;
+        }
+    }
 
+    public boolean mettreAJourFournisseur(Fournisseur fournisseur) {
+        String update = "UPDATE fournisseur SET nom_fournisseur=?,adresse_fournisseur=?,mail_fournisseur=?,telephone_fournisseur=?, " +
+                "delai_livraison_moyen=?, frais_livraison=? WHERE id_fournisseur=?";
+        try {
+            PreparedStatement stmt = this.cnx.prepareStatement(update);
+            stmt.setString(1,fournisseur.getNomfournisseur() );
+            stmt.setString(2, fournisseur.getAdresseFournisseur() );
+            stmt.setString(3, fournisseur.getMailFournisseur() );
+            stmt.setString(4, fournisseur.getTelephoneFournisseur());
+            stmt.setInt(5, fournisseur.getDelaiLivraisionMoyen() );
+            stmt.setDouble(6,fournisseur.getFraisLivraison());
+            stmt.setInt(7,fournisseur.getIdFournisseur());
+            stmt.executeUpdate();
+            return true;
+        } catch (SQLException e) {
+            System.out.println("Erreur lors de la mise a jour du fournisseur : " + '\n' + " >>" + e.getMessage());
+            return false;
+        }
+    }
 
-}
+    }
