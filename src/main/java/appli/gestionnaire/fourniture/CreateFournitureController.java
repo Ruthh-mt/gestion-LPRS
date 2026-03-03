b0a3e057de38897469322feeb2b98bbd471db2e4
Aereises <ruthmetayer0@gmail.com>
2026-03-03 08:28:18 +0100

    feat: fin du crud des fournitures

--- src/main/java/appli/gestionnaire/fourniture/CreateFournitureController.java
+++ src/main/java/appli/gestionnaire/fourniture/CreateFournitureController.java
@@ -1,4 +1,77 @@
 package appli.gestionnaire.fourniture;
 
+import appli.StartApplication;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import model.gestionnaire.Fourniture;
+import repository.gestionnaire.FournitureRepository;
+
+import java.io.IOException;
+import java.util.Optional;
+
 public class CreateFournitureController {
+
+    @FXML
+    private TextArea descriptionField;
+
+    @FXML
+    private Label errorLabel;
+
+    @FXML
+    private TextField libelleField;
+
+    @FXML
+    private TextField stockActuelleField;
+
+    @FXML
+    private TextField stockMinimumField;
+
+    @FXML
+    void onAddFourniture(ActionEvent event) throws IOException {
+        if(libelleField.getText().isEmpty() || stockActuelleField.getText().isEmpty() || stockMinimumField.getText().isEmpty()
+        || descriptionField.getText().isEmpty()){
+            showAlert(Alert.AlertType.WARNING,"Veuillez remplir tous les champs");
+        }else{
+            Fourniture fourniture = new Fourniture(
+                    libelleField.getText(),
+                    descriptionField.getText(),
+                    Integer.parseInt(stockActuelleField.getText()),
+                    Integer.parseInt(stockMinimumField.getText())
+            );
+            FournitureRepository fournitureRepo=new FournitureRepository();
+            boolean success=fournitureRepo.createFourniture(fourniture);
+            if(success){
+                Optional<ButtonType>choice=showAlertConfirmation(Alert.AlertType.CONFIRMATION,"La fourniture a bien été crée. Voulez vous ajouter un ou des fournisseurs ?.");
+                if(choice.equals(ButtonType.OK)){
+                    StartApplication.changeScene("fournitureFournisseur/addFournisseur","liaison avec Fournisseur");
+                }else{
+                    StartApplication.changeScene("gestionnaire/accueilGestionnaire","Control Center");
+                }
+
+            }else{
+                showAlert(Alert.AlertType.ERROR,"Erreur lors de l'ajout de la fourniture");
+            }
+
+        }
+    }
+
+    @FXML
+    void onReturnAccueilGestionnaire(ActionEvent event) throws IOException {
+        StartApplication.changeScene("gestionnaire/AccueilGestionnaire","Control Center");
+    }
+    private void showAlert(Alert.AlertType type, String message) {
+        Alert alert = new Alert(type);
+        alert.setTitle("Ajout Fourniture");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+    private Optional<ButtonType> showAlertConfirmation(Alert.AlertType type, String message) {
+        Alert alert = new Alert(type);
+        alert.setTitle("Ajout Fourniture");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        return alert.showAndWait();
+    }
 }
--- src/main/java/repository/gestionnaire/FournitureRepository.java
+++ src/main/java/repository/gestionnaire/FournitureRepository.java
@@ -1,9 +1,7 @@
 package repository.gestionnaire;
 
 import database.Database;
-import model.gestionnaire.Fournisseur;
 import model.gestionnaire.Fourniture;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -15,15 +13,9 @@ public class FournitureRepository {
     public FournitureRepository() {
         this.cnx = Database.getConnexion();;
     }
-    private int idFourniture;
-    private String libelle;
-    private String description;
-    private int stockActuelle;
-    private int stockMinimum;
 
     public boolean createFourniture(Fourniture fourniture) {
-        String add="INSERT INTO fourniture (libelle,description,stock_actuelle" +
-                ",stock_minimum) VALUES (?,?,?,?)";
+        String add="INSERT INTO fourniture (libelle,description,stock_actuelle,stock_minimum) VALUES (?,?,?,?)";
         try {
             PreparedStatement ps= cnx.prepareStatement(add);
             ps.setString(1,fourniture.getLibelle());
@@ -33,7 +25,7 @@ public class FournitureRepository {
             ps.executeUpdate();
             return true;
         } catch (SQLException e) {
-            System.out.println( "Erreur lors de l'ajout du Fournisseur : "+e.getMessage());
+            System.out.println( "Erreur lors de l'ajout de la Fourniture : "+e.getMessage());
             return false;
         }
 
@@ -58,9 +50,64 @@ public class FournitureRepository {
             return allFournitures;
 
         }catch(SQLException e){
-            System.out.println( "Erreur lors de la recuperation des fournitures: "+e.getMessage());
+            System.out.println( "Erreur lors de la recuperation des Fourniture : "+e.getMessage());
             return null;
         }
 
     }
+
+    public Fourniture getFournitureById(Fourniture fourniture) {
+        String get = "SELECT * FROM fourniture WHERE id_fourniture=? ";
+        try {
+            PreparedStatement stmt = this.cnx.prepareStatement(get);
+            stmt.setInt(1, fourniture.getIdFourniture());
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                 fourniture= new Fourniture (
+                        rs.getInt("id_fourniture"),
+                        rs.getString("libelle"),
+                        rs.getString("description"),
+                        rs.getInt("stock_actuelle"),
+                        rs.getInt("stock_minimum")
+                );
+            }
+            return fourniture;
+        } catch (SQLException e) {
+            System.out.println("Erreur lors de la recherche de la Fourniture: " + '\n' + " >>" + e.getMessage());
+            return null;
+        }
+    }
+
+
+    public boolean supprimerFournitureParId(Fourniture fourniture) {
+        String delete = "DELETE FROM fourniture WHERE id_fourniture=? ";
+        try {
+            PreparedStatement stmt = this.cnx.prepareStatement(delete);
+            stmt.setInt(1, fourniture.getIdFourniture());
+            stmt.executeUpdate();
+            System.out.println("Fourniture deleted with success : " + '\n' + ">> Fourniture was : " + fourniture.getLibelle());
+            return true;
+        } catch (SQLException e) {
+            System.out.println("Erreur lors de la suppresion : " + '\n' + " >>" + e.getMessage());
+            return false;
+        }
+    }
+
+    public boolean mettreAJourFourniture(Fourniture fourniture) {
+        String update = "UPDATE Fourniture SET libelle= ? ,description=? ,stock_actuelle=? ,stock_minimum=?  WHERE id_fourniture=?";
+        try {
+            PreparedStatement ps = this.cnx.prepareStatement(update);
+            ps.setString(1,fourniture.getLibelle());
+            ps.setString(2,fourniture.getDescription());
+            ps.setInt(3,fourniture.getStockActuelle());
+            ps.setInt(4,fourniture.getStockMinimum());
+            ps.setInt(5,fourniture.getIdFourniture());
+            ps.executeUpdate();
+            return true;
+        } catch (SQLException e) {
+            System.out.println("Erreur lors de la mise a jour du Fourniture : " + '\n' + " >>" + e.getMessage());
+            return false;
+        }
+    }
+
 }
